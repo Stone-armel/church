9a28505bbce521ac84844b794ff5c502784bbdbe
Church Developer <developer@church.local>
2026-09-21 19:20:36 +0100

    Update Genius Pay API credentials

--- backend/config/payment.php
+++ backend/config/payment.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  */
 return [
     'base_url'   => getenv('GENIUS_PAY_BASE_URL') ?: 'https://geniuspay.ci/api/v1/merchant/payments',
-    'api_key'    => getenv('GENIUS_PAY_API_KEY')  ?: 'sk_sandbox_ypzL78AdXWN84BGisvCnRAOhM5TpVUff',
-    'secret_key' => getenv('GENIUS_PAY_SECRET')   ?: 'ss_sandbox_XFejDutqLESacuni78QtiWIgEtoCfcrgWzcTlZCeGH4NCRKE',
+    'api_key'    => getenv('GENIUS_PAY_API_KEY')  ?: 'sk_sandbox_GH3OjAa8g2ividEVgUNyvrMD4h8blej9',
+    'secret_key' => getenv('GENIUS_PAY_SECRET')   ?: 'ss_sandbox_jfcTUNSMMaNOh2thLDoxdviD0fnNCoGMQySGDQCCZ0Bzs8pV',
     'currency'   => 'XOF',
 ];
